797aa0e8494ff5178c5a90fbd0e4913c9a70941a
Cayde Dixon <me@cazzar.net>
2017-01-08 22:14:59 +1100

    Add some general metadata to work from with Shoko
    Also derive further information via Hama (https://github.com/ZeroQI/Hama.bundle).

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -29,9 +29,13 @@ def HttpPost(url, postdata):
     myheaders = {'Content-Type': 'application/json'}
     return JSON.ObjectFromString(HTTP.Request('http://%s:%s/%s' % (Prefs['Hostname'], Prefs['Port'], url), headers=myheaders, data=postdata).content)
 
-def HttpReq(url):
+def HttpReq(url, authenticate = True):
     Log("Requesting: %s" % url)
-    return JSON.ObjectFromString(HTTP.Request('http://%s:%s/%s' % (Prefs['Hostname'], Prefs['Port'], url)).content)
+    apistring = ''
+    if authenticate:
+        apistring = '&apikey=%s' % GetApiKey()
+
+    return JSON.ObjectFromString(HTTP.Request('http://%s:%s/%s%s' % (Prefs['Hostname'], Prefs['Port'], url, apistring)).content)
 
 class ShokoTVAgent(Agent.TV_Shows):
     name = 'Shoko' 
@@ -39,27 +43,50 @@ class ShokoTVAgent(Agent.TV_Shows):
     primary_provider = True 
     fallback_agent = False 
     accepts_from = None 
-    contributes_to = None
+    contributes_to = ['com.plexapp.agents.hama']
 
     def search(self, results, media, lang):
         name = media.show
+        
         #http://127.0.0.1:8111/api/serie/search?query=Clannad&level=1&apikey=d422dfd2-bdc3-4219-b3bb-08b85aa65579
 
-        pelimresults = HttpReq("api/serie/search?query=%s&level=%d&apikey=%s" % (urllib.quote(name), 0, GetApiKey()))
-
+        pelimresults = HttpReq("api/serie/search?query=%s&level=%d" % (urllib.quote(name), 0))
+        i = 0
         for result in pelimresults:
-            score = 100 if result['title'] == name else 85 #TODO: Improve this to respect synonyms.
-            results.Append(MetadataSearchResult(result.id, result.title, score, Locale.CurrentLocale))
+            score = 100 if result['title'] == name else 85 #TODO: Improve this to respect synonyms./
+            meta = MetadataSearchResult('%s' % result['id'], result['title'], result['year'], score, lang)
+            results.Append(meta)
 
-        results.Sort('score', descending=True)
+        #results.Sort('score', descending=True)
 
     def update(self, metadata, media, lang):
-        Log("update(%s)" % media.id)
-        title = media.name
+        Log("update(%s)" % metadata.id)
+        aid = metadata.id
+        # title = media.name
         #http://127.0.0.1:8111/api/ep/getbyfilename?apikey=d422dfd2-bdc3-4219-b3bb-08b85aa65579&filename=%5Bjoseole99%5D%20Clannad%20-%2001%20(1280x720%20Blu-ray%20H264)%20%5B8E128DF5%5D.mkv
 
-        episode_data = HttpReq("api/ep/getbyfilename?apikey=%s&filename=%s" % (GetApiKey(), urllib.quote(media.filename)))
-        
-        metadata.title = episode_data['title']
-        metadata.summary = episode_data['summary']
+        # episode_data = HttpReq("api/ep/getbyfilename?apikey=%s&filename=%s" % (GetApiKey(), urllib.quote(media.filename)))
+        series = HttpReq("api/serie?id=%s" % aid)
 
+        #build metadata on the TV show.
+        metadata.summary = series['summary']
+        metadata.title = series['title']
+        metadata.rating = float(series['rating'])
+
+        tags = []
+        for tag in series['tags']:
+            tags.append(tag['tag'])
+
+        metadata.tags = tags
+
+        if (len(series['art']['banner'])):
+            for art in series['art']['banner']:
+                metadata.banner[art['url']] = Proxy.Media(HTTP.Request(art['url']).content, art['index'])
+
+        if (len(series['art']['thumb'])):
+            for art in series['art']['thumb']:
+                metadata.posters[art['url']] = Proxy.Media(HTTP.Request(art['url']).content, art['index'])
+
+        if (len(series['art']['fanart'])):
+            for art in series['art']['fanart']:
+                metadata.art[art['url']] = Proxy.Media(HTTP.Request(art['url']).content, art['index'])
